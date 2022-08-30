4f70995af59db3802dcef970f1f43ef77e4c3bb3
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2022-08-31 00:50:25 +0900

    dice bot2

---
+++ Bot/slackbot_settings.py
@@ -0,0 +1,8 @@
+API_TOKEN = "xoxb-4013899084772-4013917706788-hkESRPYqjnG7djjK1wC5iHL3"
+#토큰 대입
+
+# 실제로 작성한 코드는 dice_bot모듈에 있음
+
+PLUGINS = ['dice_bot']
+# 모듈, 패키지 모두 넣을 수 있음.
+# 패키지하면 하위 모듈을 모두 가져옴
