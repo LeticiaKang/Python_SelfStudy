943f02b12050324d58b94767c9a7fa0ddb176d20
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2022-10-01 13:28:23 +0900

    Slack_Bot_Study
    Slack을 이용해서 Bot만들기

---
+++ ch12. SlackBot/slackbot_settings.py
@@ -0,0 +1,10 @@
+API_TOKEN = "xoxb-4013899084772-4059207413078-9fB1QYi721IFtzegupvTALQR"
+#토큰 대입
+
+# 실제로 작성한 코드는 dice_bot모듈에 있음
+
+# DEFAULT_REPLY = "안녕하세요. 테스트봇입니다."
+
+PLUGINS = ['dice_bot']
+# 모듈, 패키지 모두 넣을 수 있음.
+# 패키지하면 하위 모듈을 모두 가져옴
