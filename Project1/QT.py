c7c659133bd9fe990b1c7fa7e252e3994fe3e023
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2022-11-06 23:45:47 +0900

    python_creat_크롤링exe
    비밀번호 asterick
    카페 크롤링 -> 뉴스크롤링
    gui 변경 ( 제목, url ) 가져와서 나타내기
    gui에 제목, url 표시 성공
    
    - 안되는 부분
    1. exe 계속 오류남
    2. exe 만드는데 오래 걸림

--- Project1/QT.py
+++ Project1/QT.py
@@ -1,11 +1,19 @@
-# pyinstaller -w -F NaverCrawlingUI.py
-# pyinstaller NaverCrawlingUI.py
+# pyi-makespec --noconsole --onefile QT.py
+# pyinstaller QT.spec
+# 실행파일 만들기 : Python 파일이 있는 폴더로 이동한 다음, 아래 명령어를 입력하면 해당 폴더에 실행파일이 만들어집니다.
+# pyinstaller QT.py
+# 콘솔창 없이 실행파일 하나만
+# pyinstaller -w -F QT.py
+# pyinstaller -w --onefile QT.py
+# 수정된 스팩파일 빌드 pyinstaller QT.spec
 
 #1) 라이브러리 import
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+from PyQt5 import QtWidgets
 import sys
 import os
+import NaverCrawling as crawler
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
@@ -22,23 +30,35 @@ class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
+        self.lineEdit_ID.setPlaceholderText("Enter ID Here")
+        self.lineEdit_PWD.setPlaceholderText("Enter Password Here")
+        self.lineEdit_PWD.setEchoMode(QtWidgets.QLineEdit.Password)
 
-    #버튼에 기능을 연결하는 코드
-        self.btn1.clicked.connect(self.button1Function)
-        self.btn2.clicked.connect(self.button2Function)
+    # 버튼 이벤트
+        self.btn_login.clicked.connect(self.button2Function)
+        self.btn_hankuk.clicked.connect(self.button1Function)
+        self.btn_meil.clicked.connect(self.button1Function)
 
-    # TextEdit과 관련된 버튼에 기능 연결
-        self.inputID_2.toPlainText()
-        self.inputPWD.toPlainText()
+    # LineEdit과 관련된 버튼에 기능 연결
+        id = self.lineEdit_ID.text()
+        pwd = self.lineEdit_PWD.text()
+
+    # TableWidget에 크롤링 내용 연동하기
+
+        for row,title,url in zip(range(10), crawler.title_list, crawler.url_list):
+            self.tableWidget_contents.setItem(row, 0, QTableWidgetItem(title))
+            self.tableWidget_contents.setItem(row, 1, QTableWidgetItem(url))
+
 
 #btn_1이 눌리면 작동할 함수
     def button1Function(self) :
-        print("btn1 Clicked")
+        print("btn Clicked")
 #btn_2가 눌리면 작동할 함수
     def button2Function(self) :
-        print("btn2 Clicked")
-        print(self.inputID_2.toPlainText())
-        print(self.inputPWD.toPlainText())
+        print("btn Clicked")
+        print(self.lineEdit_ID.text()) # Lineedit에 있는 글자를 가져오는 메서드
+        print(self.lineEdit_PWD.text())
+
 
 
 
@@ -49,6 +69,9 @@ if __name__ == '__main__':
 
     # WindowClass의 인스턴스 생성
     myWindow = WindowClass()
+    # pw = QtGui.QLineEdit()
+    # pw.setEchoMode(QtGui.QLineEdit.Password)
+    # pw.show()
 
     # 프로그램 화면을 보여주는 코드
     myWindow.show()
